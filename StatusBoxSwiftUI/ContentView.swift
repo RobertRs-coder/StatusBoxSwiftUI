283a64814187d25d9683f82cd3fb8a5278625268
Roberto Rojo <rrojo.va@gmail.com>
2022-11-03 05:16:13 +0100

    Initial Commit

---
+++ StatusBoxSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StatusBoxSwiftUI
+//
+//  Created by Roberto Rojo Sahuquillo on 3/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StatusBoxSwiftUI/StatusBoxSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusBoxSwiftUIApp.swift
+//  StatusBoxSwiftUI
+//
+//  Created by Roberto Rojo Sahuquillo on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct StatusBoxSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
