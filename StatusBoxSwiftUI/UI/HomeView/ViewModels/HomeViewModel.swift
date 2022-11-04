7c751ca3d157f60646e9e9e5c085c0b3db1f988c
Roberto Rojo <rrojo.va@gmail.com>
2022-11-04 11:03:06 +0100

    Create Basic MVVM Structure

---
+++ StatusBoxSwiftUI/UI/HomeView/ViewModels/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  StatusBoxSwiftUI
+//
+//  Created by Roberto Rojo Sahuquillo on 4/11/22.
+//
+
+import Foundation
---
+++ StatusBoxSwiftUI/UI/HomeView/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  StatusBoxSwiftUI
+//
+//  Created by Roberto Rojo Sahuquillo on 4/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
